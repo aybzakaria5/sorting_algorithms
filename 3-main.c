81df4b8aa1244c4fcb1544379c09dd22c91320e1
faty <fatyboubakri31@gmail.com>
2023-09-27 20:58:38 +0000

    fix checks

--- 3-main.c
+++ 3-main.c
@@ -9,7 +9,8 @@
  */
 int main(void)
 {
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    int array[] = {1, 12, 14, 15, 25, 17, 8, 9, 10, 28,
+    13, 100, 103, 105, 29, 3, 7, 6, 0, 11};
     size_t n = sizeof(array) / sizeof(array[0]);
 
     print_array(array, n);
